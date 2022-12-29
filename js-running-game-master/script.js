8eb89e64636ebad73f25f2e1760f250c545669b8
Diksha Tomar <Dikshatomar1511@gmail.com>
2022-12-30 00:32:51 +0530

    Add files via upload

---
+++ js-running-game-master/script.js
@@ -0,0 +1,32 @@
+const hero = document.querySelector(".hero");
+const heroBoy = document.querySelector(".hero-boy");
+const villain = document.querySelector(".villain");
+
+function jump() {
+    if (hero.classList != "animate") {
+        hero.classList.add("animate");
+        villain.style.animation = "move 1s infinite linear";
+    }
+
+    setTimeout(function() {
+        hero.classList.remove("animate");
+    }, 300);
+}
+
+document.addEventListener("keydown", function() {
+    jump();
+})
+
+let isAlive = setInterval(function() {
+    let heroTop = parseInt(window.getComputedStyle(hero).getPropertyValue("top"));
+    //   console.log(heroTop);
+    let villainLeft = parseInt(window.getComputedStyle(villain).getPropertyValue("left"));
+
+    if (villainLeft < 40 && villainLeft > 20 && heroTop >= 130) {
+        villain.style.animation = "none";
+
+        alert("Game Over. Press spacebar to restart");
+    }
+
+
+}, 10);
